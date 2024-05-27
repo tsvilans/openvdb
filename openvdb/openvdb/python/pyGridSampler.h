282083bb46f804eefcbe187227c00bcdc238aacc
Tom Svilans <tom.svilans@gmail.com>
2024-05-27 16:25:18 +0200

    fix(pyGridSampler): cleanup

--- openvdb/openvdb/python/pyGridSampler.h
+++ openvdb/openvdb/python/pyGridSampler.h
@@ -17,7 +17,7 @@ namespace pyGridSampler {
 
 
     //@{
-    /// Type traits for grid accessors
+    /// Type traits for grid samplers
     template<typename _GridT, typename _SamplerT>
     struct GridSamplerTraitsBase
     {
@@ -26,7 +26,6 @@ namespace pyGridSampler {
 
         using NonConstGridT = GridT;
         using GridPtrT = typename NonConstGridT::Ptr;
-        //using AccessorT = typename NonConstGridT::Accessor;
         using ValueT = typename GridT::ValueType;
 
         static const bool IsConst = false;
@@ -57,30 +56,15 @@ namespace pyGridSampler {
     ////////////////////////////////////////
 
 
-    /// @brief ValueAccessor wrapper class that also stores a grid pointer,
+    /// @brief GridSampler wrapper class that also stores a grid pointer,
     /// so that the grid doesn't get deleted as long as the accessor is live
     ///
-    /// @internal This class could have just been made to inherit from ValueAccessor,
-    /// but the method wrappers allow for more Pythonic error messages.  For example,
-    /// if we constructed the Python getValue() method directly from the corresponding
-    /// ValueAccessor method, as follows,
-    ///
-    ///    .def("getValue", &Accessor::getValue, ...)
-    ///
-    /// then the conversion from a Python type to a Coord& would be done
-    /// automatically.  But if the Python method were called with an object of
-    /// a type that is not convertible to a Coord, then the TypeError message
-    /// would say something like "TypeError: No registered converter was able to
-    /// produce a C++ rvalue of type openvdb::math::Coord...".
-    /// Handling the type conversion manually is more work, but it allows us to
-    /// instead generate messages like "TypeError: expected tuple(int, int, int),
-    /// found str as argument to FloatGridAccessor.getValue()".
+    /// @internal This class is directly adapted from ValueAccessorWrap
     template<typename _GridType, typename _SamplerType>
     class GridSamplerWrap
     {
     public:
         using Traits = GridSamplerTraits<_GridType, _SamplerType>;
-        //using GridSampler = typename Traits::AccessorT;
         using ValueType = typename Traits::ValueT;
         using GridType = typename Traits::NonConstGridT;
         using GridPtrType = typename Traits::GridPtrT;
@@ -121,15 +105,15 @@ namespace pyGridSampler {
 
             py::class_<GridSamplerWrap>(m,
                 (pyGridTypeName + pyGridSamplerTypeName).c_str(), //pybind11 requires a unique class name for each template instantiation
-                ("Read/write access by (i, j, k) index coordinates to the voxels\nof a "
+                ("Class that provides the interface for continuous sampling of \nvalues in a "
                     + pyGridTypeName).c_str())
                 .def(py::init<const GridPtrType&>(), py::arg("grid"),
                     "Initialize with a grid to be sampled.")
                 .def("copy", &GridSamplerWrap::copy,
                     ("copy() -> " + pyGridSamplerTypeName + "\n\n"
-                        "Return a copy of this accessor.").c_str())
+                        "Return a copy of this grid sampler.").c_str())
                 .def_property_readonly("parent", &GridSamplerWrap::parent,
-                    ("this accessor's parent " + pyGridTypeName).c_str())
+                    ("this grid sampler's parent " + pyGridTypeName).c_str())
 
                 //
                 // Voxel access
@@ -137,20 +121,20 @@ namespace pyGridSampler {
                 .def("isSample", &GridSamplerWrap::isSample,
                     py::arg("ijk"),
                     ("isSample(ijk) -> " + pyValueTypeName + "\n\n"
-                        "Return the value of the voxel in index space at coordinates (i, j, k).").c_str())
+                        "Return the index-space value at coordinates (i, j, k).").c_str())
                 .def("wsSample", &GridSamplerWrap::wsSample,
-                    py::arg("ijk"),
-                    ("wsSample(ijk) -> " + pyValueTypeName + "\n\n"
-                        "Return the value of the voxel in world space at coordinates (i, j, k).").c_str())
+                    py::arg("xyz"),
+                    ("wsSample(xyz) -> " + pyValueTypeName + "\n\n"
+                        "Return the world-space value at coordinates (x, y, z).").c_str())
 
-                ; // py::class_<ValueAccessor>
+                ; // py::class_<GridSampler>
         }
 
     private:
         const GridPtrType mGrid;
         openvdb::tools::GridSampler<GridType, _SamplerType> mSampler;
-    }; // class AccessorWrap
+    }; // class GridSamplerWrap
 
-} // namespace pyAccessor
+} // namespace pyGridSampler
 
 #endif // OPENVDB_PYGRIDSAMPLER_HAS_BEEN_INCLUDED
